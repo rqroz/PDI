ed7fa81470b3dbdad28ab2c276a873c1c4a19fbe
rqroz <rqroz1@gmail.com>
2018-04-06 03:11:32 -0300

    tiltshift complete

--- SpatialDomainFiltering/TiltShift/TiltShift/tiltshift.cpp
+++ SpatialDomainFiltering/TiltShift/TiltShift/tiltshift.cpp
@@ -1,31 +1,29 @@
 
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <math.h>
 
 #define MAX_SLIDER_VAL 100
 
 using namespace cv;
 using namespace std;
 
-int heightSlider = 0, decaySlider = 0, vertSlider = 0, alphaSlider = 0;
+int heightSlider = 0, decaySlider = 0, vertSlider = 0;
 double heightVal, decayVal, vertVal;
-double alpha = 0.5;
 /*
 * image1: original image
 * image2: image after low pass filtering
 * imageTop:
 * blended: resulting image after merge of image with imageTop
 */
-Mat image1, image2, imageTop, blended;
 Mat originalImage, lowPassFilteredImage, mergedImage, imagesContainer;
-
-char trackbarName[50];
+Size imgSize;
 
 void initializeContainer();
 void addTextToImage(Mat&, String, float);
-void mergeImages(Mat&, Mat&, Mat&);
+void populateWeightMats(Mat&, Mat&);
+void mergeImages();
 
-void onTrackbarBlend(int, void*);
 void onTrackbarHeight(int, void*);
 void onTrackbarDecay(int, void*);
 void onTrackbarVert(int, void*);
@@ -44,6 +42,8 @@ void lowPassFilter(Mat &src, Mat &dst){
 }
 
 int main(int argc, char* argv[]){
+  char trackbarName[50];
+
   originalImage = imread("trainspotting.jpg");
 
   if(!originalImage.data){
@@ -51,92 +51,101 @@ int main(int argc, char* argv[]){
     exit(1);
   }
 
-  namedWindow("tiltShift", 1);
+  resize(originalImage, originalImage, Size(480, 320));
 
+  namedWindow("tiltShift", 1);
+  
   lowPassFilter(originalImage, lowPassFilteredImage);
   initializeContainer();
-  mergeImages(originalImage, lowPassFilteredImage, mergedImage);
-
-  imshow("tiltShift", imagesContainer);
-
-  sprintf(trackbarName, "Alpha x %d", MAX_SLIDER_VAL);
-  createTrackbar(trackbarName, "tiltShift", &alphaSlider, MAX_SLIDER_VAL, onTrackbarBlend);
-  onTrackbarBlend(alphaSlider, 0);
+  mergeImages();
 
   sprintf(trackbarName, "Region Height x %d", MAX_SLIDER_VAL);
   createTrackbar(trackbarName, "tiltShift", &heightSlider, MAX_SLIDER_VAL, onTrackbarHeight);
-  onTrackbarHeight(heightSlider, 0);
-
-  sprintf(trackbarName, "Decay x %d", MAX_SLIDER_VAL);
-  createTrackbar(trackbarName, "tiltShift", &decaySlider, MAX_SLIDER_VAL, onTrackbarDecay);
-  onTrackbarDecay(decaySlider, 0);
 
   sprintf(trackbarName, "Vertical Position x %d", MAX_SLIDER_VAL);
   createTrackbar(trackbarName, "tiltShift", &vertSlider, MAX_SLIDER_VAL, onTrackbarVert);
-  onTrackbarVert(vertSlider, 0);
+
+  sprintf(trackbarName, "Decay");
+  createTrackbar(trackbarName, "tiltShift", &decaySlider, MAX_SLIDER_VAL, onTrackbarDecay);
 
   waitKey(0);
-
+  imwrite("result.png", imagesContainer);
   return 0;
 }
 
 void initializeContainer(){
-  Size imgSize = originalImage.size(), halfSize = imgSize/2;
+  imgSize = originalImage.size();
+  Size halfSize = imgSize/2;
   Mat smallerImage;
 
   imagesContainer = Mat(1.5*imgSize.height, imgSize.width, CV_8UC3);
 
   resize(originalImage, smallerImage, halfSize);
-  addTextToImage(smallerImage, "Original", halfSize.width/3);
+  addTextToImage(smallerImage, "Original", 5);
   smallerImage.copyTo(imagesContainer(Rect(0, 0, halfSize.width, halfSize.height)));
   resize(lowPassFilteredImage, smallerImage, halfSize);
-  addTextToImage(smallerImage, "Low Pass Filter", halfSize.width/4);
+  addTextToImage(smallerImage, "Low Pass Filter", 14);
   smallerImage.copyTo(imagesContainer(Rect(halfSize.width, 0, halfSize.width, halfSize.height)));
 }
 
-void addTextToImage(Mat& img, String text, float width){
-  double fontScale = 0.75;
-  int thickness = 2;
+void addTextToImage(Mat& img, String text, float margin){
+  float fontScale = 0.75, thickness = 1.5;
   Size s = img.size();
-  Point textOrg(width, s.height/1.1);
-  putText(img, text, textOrg, FONT_HERSHEY_TRIPLEX, fontScale, Scalar(0, 0, 255), thickness,8);
+  Point textOrg(s.width/margin, s.height/1.1);
+  putText(img, text, textOrg, FONT_HERSHEY_TRIPLEX, fontScale, Scalar(0, 127, 255), thickness, 8);
 }
 
-void onTrackbarBlend(int, void*){
-  alpha = (double) alphaSlider/MAX_SLIDER_VAL;
-  mergeImages(originalImage, lowPassFilteredImage, mergedImage);
-  imshow("tiltShift", mergedImage);
+void populateWeightMats(Mat& alphaMat, Mat& betaMat){
+  float alpha = 0.5, beta = 1 - alpha;
+  int x, upperBound, lowerBound;
+  upperBound = -heightVal/2;
+  lowerBound = -upperBound;
+
+  for(int i = 0; i < imgSize.height; ++i){
+    x = i - (vertVal + heightVal/2);
+    alpha = 0.5*(tanh((x - upperBound)/decayVal) - tanh((x - lowerBound)/decayVal));
+    beta = 1 - alpha;
+
+    Vec3f alphaV(alpha, alpha, alpha);
+    Vec3f betaV(beta, beta, beta);
+
+    for(int j = 0; j < imgSize.width; ++j){
+      alphaMat.at<Vec3f>(i, j) = alphaV;
+      betaMat.at<Vec3f>(i, j) = betaV;
+    }
+  }
 }
-//
-// void onTrackbarLine(int, void*){
-//   image1.copyTo(imageTop);
-//   int limit = topSlider*255/MAX_SLIDER_VAL;
-//   if(limit > 0){
-//     Mat tmp = image2(Rect(0, 0, 256, limit));
-//     tmp.copyTo(imageTop(Rect(0, 0, 256, limit)));
-//   }
-//
-//   onTrackbarBlend(alphaSlider, 0);
-// }
 
-void mergeImages(Mat& image, Mat& lpfImage, Mat& result){
-  addWeighted(image, alpha, lpfImage, 1 - alpha, 0.0, result);
+void mergeImages(){
+  imgSize = originalImage.size();
+  Mat alphaMat(imgSize, CV_32FC3), betaMat(imgSize, CV_32FC3), outputOriginal, outputLPF;
 
-  Size imgSize = image.size();
-  result.copyTo(imagesContainer(Rect(0, imgSize.height/2, imgSize.width, imgSize.height)));
+  populateWeightMats(alphaMat, betaMat);
+
+  originalImage.convertTo(outputOriginal, CV_32FC3);
+  lowPassFilteredImage.convertTo(outputLPF, CV_32FC3);
+
+  multiply(outputOriginal, alphaMat, outputOriginal);
+  multiply(outputLPF, betaMat, outputLPF);
+
+  add(outputOriginal, outputLPF, mergedImage);
+
+  mergedImage.convertTo(mergedImage, CV_8UC3);
+  mergedImage.copyTo(imagesContainer(Rect(0, imgSize.height/2, imgSize.width, imgSize.height)));
+  imshow("tiltShift", imagesContainer);
 }
 
 void onTrackbarHeight(int, void*){
-  heightVal = (double) heightSlider/MAX_SLIDER_VAL;
-  cout << "Height Slider: " << heightVal << endl;
-}
-
-void onTrackbarDecay(int, void*){
-  decayVal = (double) decaySlider/MAX_SLIDER_VAL;
-  cout << "Weight Slider: " << decayVal << endl;
+  heightVal = (double) heightSlider*imgSize.height/MAX_SLIDER_VAL;
+  mergeImages();
 }
 
 void onTrackbarVert(int, void*){
-  vertVal = (double) vertSlider/MAX_SLIDER_VAL;
-  cout << "Vert Slider: " << vertVal << endl;
+  vertVal = (double) vertSlider*imgSize.height/MAX_SLIDER_VAL;
+  mergeImages();
+}
+
+void onTrackbarDecay(int, void*){
+  decayVal = decaySlider == 0 ? 0.01 : (double) decaySlider;
+  mergeImages();
 }
